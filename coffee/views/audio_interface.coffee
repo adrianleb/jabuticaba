d55eba1cbecaacefee098f52eb6463520cb330f9
Adrian le Bas <adrianleb@gmail.com>
2013-04-20 17:34:13 +0200

    KAOSSPAD TABAJARA

--- coffee/views/audio_interface.coffee
+++ coffee/views/audio_interface.coffee
@@ -6,12 +6,30 @@ class Jabuticaba.Views.AudioInterface extends Backbone.View
   # Define main element to attach to
   el: "#audio-interface"
 
+  events:
+    "mousedown #audio-generator" : "noteOn"
+    "mouseup #audio-generator" : "noteOff"
   initialize: ->
     console.debug('init audio interface')
     @render()
 
   render: ->
     @$el.append(@template())
+
+
+  # SOUND GENERATOR
+  noteOn: (e) ->
+    elW =  e.currentTarget.clientWidth
+    elH = e.currentTarget.clientHeight
+    eX = e.offsetX
+    eY = e.offsetY
+    coord = {x: eX / elW, y: eY / elH }
+    console.log coord
+    $(window).trigger 'audio_gen_note_on', coord
+    @$(e.currentTarget).find('.audio_module_point').css({top:(coord.y * 100)+ "%", left:(coord.x * 100) + "%"})
+
+  noteOff: (e) ->
+    $(window).trigger 'audio_gen_note_off'
 
 
 
--- public/js/views/audio_interface.js
+++ public/js/views/audio_interface.js
@@ -15,6 +15,11 @@
 
     AudioInterface.prototype.el = "#audio-interface";
 
+    AudioInterface.prototype.events = {
+      "mousedown #audio-generator": "noteOn",
+      "mouseup #audio-generator": "noteOff"
+    };
+
     AudioInterface.prototype.initialize = function() {
       console.debug('init audio interface');
       return this.render();
@@ -22,6 +27,28 @@
 
     AudioInterface.prototype.render = function() {
       return this.$el.append(this.template());
+    };
+
+    AudioInterface.prototype.noteOn = function(e) {
+      var coord, eX, eY, elH, elW;
+      elW = e.currentTarget.clientWidth;
+      elH = e.currentTarget.clientHeight;
+      eX = e.offsetX;
+      eY = e.offsetY;
+      coord = {
+        x: eX / elW,
+        y: eY / elH
+      };
+      console.log(coord);
+      $(window).trigger('audio_gen_note_on', coord);
+      return this.$(e.currentTarget).find('.audio_module_point').css({
+        top: (coord.y * 100) + "%",
+        left: (coord.x * 100) + "%"
+      });
+    };
+
+    AudioInterface.prototype.noteOff = function(e) {
+      return $(window).trigger('audio_gen_note_off');
     };
 
     return AudioInterface;
