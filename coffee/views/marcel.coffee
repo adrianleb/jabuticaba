265b13cd02cb4d4dcef58babfc44b34c71581957
Marcel Corso <marcel.corso@gmail.com>
2013-04-20 16:58:07 +0200

    start with channels

--- coffee/views/marcel.coffee
+++ coffee/views/marcel.coffee
@@ -6,11 +6,42 @@ class Jabuticaba.Views.Marcel extends Backbone.View
   # Define main element to attach to
   el: "body"
 
+  events:
+    '#send click': 'send'
+    '#open-room': 'openRoom'
+    '#join-room': 'joinRoom'
+
+
   initialize: ->
-    console.debug('initialize marcel')
+    console.debug('init marcelzz')
+    @room = new DataChannel('room')
+
+    @room.onopen = (user_id) =>
+      console.log('onopen: ' + user_id)
+
+    @room.onUserLeft = (user_id) =>
+      console.log('onUserLeft: ' + user_id)
+
+    @room.onmessage = (msg) =>
+      @$('#messages').append(msg + "<br/>")
+
+
+    @render()
+
 
   render: ->
     @$el.find('#container').append(@template())
 
+
+  send: ->
+    @room.send(@$('#msg').val())
+
+  
+  openRoom: ->
+    # create the room
+    @room.open('room')
+
+  joinRoom: ->
+    @room.connect('room')
 
 
--- public/js/jst.js
+++ public/js/jst.js
@@ -47,53 +47,6 @@ JST['home'] = function(__obj) {
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
 };
-JST['descriptions/channels/resource'] = function(__obj) {
-  if (!__obj) __obj = {};
-  var __out = [], __capture = function(callback) {
-    var out = __out, result;
-    __out = [];
-    callback.call(this);
-    result = __out.join('');
-    __out = out;
-    return __safe(result);
-  }, __sanitize = function(value) {
-    if (value && value.ecoSafe) {
-      return value;
-    } else if (typeof value !== 'undefined' && value != null) {
-      return __escape(value);
-    } else {
-      return '';
-    }
-  }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
-  __safe = __obj.safe = function(value) {
-    if (value && value.ecoSafe) {
-      return value;
-    } else {
-      if (!(typeof value !== 'undefined' && value != null)) value = '';
-      var result = new String(value);
-      result.ecoSafe = true;
-      return result;
-    }
-  };
-  if (!__escape) {
-    __escape = __obj.escape = function(value) {
-      return ('' + value)
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;');
-    };
-  }
-  (function() {
-    (function() {
-      __out.push('<p>Channel responses follow the Activity Streams JSON specification (http://activitystrea.ms/specs/json/1.0/).\n<br/>\nIn its simplest form, an activity consists of an actor, a verb, an object and a target. \n<ul>\n  <li>on genre channels the actor is “site”, the verb is “post”, the object is “track” and the target is implicit.</li>\n  <li>on site channels, the actor will always be the same.</li>\n</p>\n\n<h2>List of channel keys</h2>\n<ul>\n  <li>\n    <h3>Genre channels</h3>\n    <ul>\n      <li>/channels/idm</li>\n      <li>/channels/uk+garage</li>\n    </ul>\n  </li>\n  <li>\n    <h3>Media channels</h3>\n    <ul>\n      <li>\n        <b>MP3 tracks:</b> /channels/media:mp3\n      </li>\n      <li><b>YouTube tracks:</b> /channels/media:youtube</li>\n      <li><b>Vimeo tracks:</b> /channels/media:vimeo</li>\n      <li><b>SoundCloud tracks:</b> /channels/media:soundcloud</li>\n      <li><b>Official.fm tracks:</b> /channels/media:officialfm</li>\n    </ul>\n  </li>\n  <li>\n    <h3>Site channels</h3>\n    <ul>\n      <li>/channels/site:37</li>\n    </ul>\n  </li>\n  <li>\n    <h3>Artist channels</h3>\n    <ul>\n      <li>/channels/artist:419</li>\n    </ul>\n  </li>\n</ul>');
-    
-    }).call(this);
-    
-  }).call(__obj);
-  __obj.safe = __objSafe, __obj.escape = __escape;
-  return __out.join('');
-};
 JST['descriptions/channels/by_key'] = function(__obj) {
   if (!__obj) __obj = {};
   var __out = [], __capture = function(callback) {
@@ -470,7 +423,7 @@ JST['descriptions/authorizations/description'] = function(__obj) {
   __obj.safe = __objSafe, __obj.escape = __escape;
   return __out.join('');
 };
-JST['resource'] = function(__obj) {
+JST['marcel'] = function(__obj) {
   if (!__obj) __obj = {};
   var __out = [], __capture = function(callback) {
     var out = __out, result;
@@ -509,101 +462,8 @@ JST['resource'] = function(__obj) {
   }
   (function() {
     (function() {
-      var method, method_desc, resource_desc, _i, _len, _ref;
     
-      __out.push('<section class="resource" id="');
-    
-      __out.push(__sanitize(this.resource.get('name').toLowerCase()));
-    
-      __out.push('">\n  <h2 class="resource_name">\n    ');
-    
-      __out.push(__sanitize(this.resource.get('name')));
-    
-      __out.push('\n  </h2>\n  <div class="resource_description">\n    ');
-    
-      if (this.resource.get('description') != null) {
-        __out.push('\n      ');
-        if (this.resource.get('description').indexOf('template:') === 0) {
-          __out.push('\n        ');
-          resource_desc = "descriptions/" + this.resource.get('description').replace('template:', '').replace(':', '/');
-          __out.push('\n        ');
-          __out.push(JST[resource_desc] != null ? JST[resource_desc]() : void 0);
-          __out.push('\n      ');
-        }
-        __out.push('\n    ');
-      }
-    
-      __out.push('\n  </div>\n  ');
-    
-      _ref = this.resource.get('methods');
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        method = _ref[_i];
-        __out.push('\n  <div class="method">\n    <h2 class="method_info">\n      <span class="method_method ');
-        __out.push(__sanitize(method.method.toLowerCase()));
-        __out.push('">');
-        __out.push(__sanitize(method.method));
-        __out.push('</span> \n      <span class="method_name">');
-        __out.push(method.name.replace(/{(.+?)}/g, '<span class="method_name_param">{$1}</span>'));
-        __out.push('</span> \n      <span class="method_return">');
-        __out.push(__sanitize(method.returns));
-        __out.push('</span> </h2>\n    <div class="method_more">\n      ');
-        __out.push('\n      <div class="method_description">\n        ');
-        if (method.description != null) {
-          __out.push('\n          ');
-          if (method.description.indexOf('template:') === 0) {
-            __out.push('\n            ');
-            method_desc = "descriptions/" + method.description.replace('template:', '').replace(':', '/');
-            __out.push('\n            ');
-            __out.push(JST[method_desc] ? JST[method_desc]() : void 0);
-            __out.push('\n            ');
-          } else {
-            __out.push('\n            <p>');
-            __out.push(__sanitize(method.description));
-            __out.push('</p>\n            ');
-          }
-          __out.push('\n        ');
-        }
-        __out.push('\n      </div>\n      ');
-        __out.push('\n      ');
-        if (method.parameters != null) {
-          __out.push('\n        <h3 class="example_lined">Parameters </h3>\n        <ul class="method_params">\n          ');
-          _.each(method.parameters, function(value, key) {
-            __out.push('\n            <li><b>');
-            __out.push(__sanitize(key));
-            __out.push('</b>: ');
-            __out.push(__sanitize(value));
-            return __out.push('</li>\n          ');
-          });
-          __out.push('\n        </ul>\n      ');
-        }
-        __out.push('\n      ');
-        __out.push('\n      ');
-        if ((method.authorization_required != null) && method.authorization_required) {
-          __out.push('\n        <div class="example auth_example gapafter_x2 gapbefore">\n          <h3 class="auth_req">authorization required</h3>\n          ');
-          if (method.scope != null) {
-            __out.push('\n            <h3 class="scope">Scope: <b>');
-            __out.push(__sanitize(method.scope));
-            __out.push('</b></h3>\n          ');
-          }
-          __out.push('\n        </div>\n      ');
-        }
-        __out.push('\n      ');
-        __out.push('\n      <h3 class="example">Example</h3>\n      <pre>$ curl "');
-        __out.push(__sanitize(method.example_url));
-        __out.push('"</pre>\n      ');
-        if (method.example_result_type === 'JSON') {
-          __out.push('\n        <h3 class="example">Result</h3>\n        <pre class="code_example">');
-          __out.push(prettyJson(method.example_result));
-          __out.push('</pre>\n      ');
-        } else if (method.example_result != null) {
-          __out.push('\n        <h3 class="example">Result</h3>\n        <pre class="code_example">');
-          __out.push(__sanitize(method.example_result.replace('"', '')));
-          __out.push('</pre>\n      ');
-        }
-        __out.push('\n    </div>\n  </div>\n  ');
-      }
-    
-      __out.push('\n\n</section>\n\n\n\n');
+      __out.push('\n<h1>Marcel</h1>\n\n<a id="open-room" href="#">Open Room</a>\n<a id="join-room" href="#">Join Room</a>\n\n<input type="text" id="msg" value="braca"></input><a id="send" href="#">Send</a>\n<br/>\n\n<div id="messages">\n</div>\n\n');
     
     }).call(this);
     
