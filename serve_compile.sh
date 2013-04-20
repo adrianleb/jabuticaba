2181568de225c35440d4cf6ee24ce88b93f37565
Adrian le Bas <adrianleb@gmail.com>
2013-04-20 15:47:43 +0200

    no json minify kthx

--- serve_compile.sh
+++ serve_compile.sh
@@ -2,7 +2,7 @@
 
 coffee --output public/js/ --watch --compile coffee/ &
 compass watch --sass-dir scss/ --css-dir public/css/ --output-style compressed --debug-info --trace &
-./resource-json-compiler.js &
+# ./resource-json-compiler.js &
 ./eco-compiler.js &
 cd public
 ponyhost server
